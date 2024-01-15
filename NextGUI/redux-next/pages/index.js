ad9710119ca6ccf7ab2ef0b0f0faeac3ede42379
Michael Crapse <mikeeybikeey@gmail.com>
2024-01-15 14:41:05 -0700

    Remove unused HTML rows

--- NextGUI/redux-next/pages/index.js
+++ NextGUI/redux-next/pages/index.js
@@ -7,15 +7,10 @@
  */
 
 import React from 'react' //React is implicitly imported
-import ProblemRow from '../components/pageblocks/ProblemRow'
 import ProblemRowReact from '../components/pageblocks/ProblemRowReact'
-import ReduceToRow from '../components/pageblocks/ReduceToRow'
 import ReduceToRowReact from '../components/pageblocks/ReduceToRowReact'
-import VisualizeRow from '../components/pageblocks/VisualizeRow'
 import VisualizeRowReact from '../components/pageblocks/VisualizeRowReact'
-import SolveRow from '../components/pageblocks/SolveRow'
 import SolveRowReact from '../components/pageblocks/SolveRowReact'
-import VerifyRow from '../components/pageblocks/VerifyRow'
 import VerifyRowReact from '../components/pageblocks/VerifyRowReact'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
