c5b25378d35565cd6754b80ba7633ef424421539
Andrija-Sevaljevic <andrijasevaljevic@isu.edu>
2025-02-03 11:41:43 -0700

    fixing warning sign

--- components/widgets/CookieConsent.js
+++ components/widgets/CookieConsent.js
@@ -71,8 +71,7 @@ function CookieConsent({ problem, solver, verifier, reducer }) {
         };
         
         setCookie("allData", JSON.stringify(allData), 30);
-        
-        event.returnValue = "Are you sure you want to leave? Your data will be saved.";
+
       }
     };
   
