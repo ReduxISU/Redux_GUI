74e3bc94681e3d0418e4aea6425f99996b5974c9
pkprathamkhanal <pkprathamkhanal@gmail.com>
2025-11-11 23:37:36 -0600

    Update Footer.jsx
    Hyperlinked ISU CS logo in footer (#40)

--- components/widgets/Footer.jsx
+++ components/widgets/Footer.jsx
@@ -1,47 +1,56 @@
-
-import React from "react"
+import React from "react";
 
 function Footer() {
-    
-    return (
-        <footer className="page-footer font-small blue pt-4">
-        <div className="container-fluid text-center text-md-left">
-            <div className="row">
-                <div className="col-md-6 mt-md-0 mt-3">
-                    <h5 className="text-uppercase">Footer Content</h5>
-                    <p>Here you can use rows and columns to organize your footer content.</p>
-                </div>
-    
-                <hr className="clearfix w-100 d-md-none pb-0"/>
-    
-                <div className="col-md-3 mb-md-0 mb-3">
-                    <h5 className="text-uppercase">Links</h5>
-                    <ul className="list-unstyled">
-                        <li><a href="#!">Link 1</a></li>
-                        <li><a href="#!">Link 2</a></li>
-                        <li><a href="#!">Link 3</a></li>
-                        <li><a href="#!">Link 4</a></li>
-                    </ul>
-                </div>
-    
-                <div className="col-md-3 mb-md-0 mb-3">
-                    <h5 className="text-uppercase">Links</h5>
-                    <ul className="list-unstyled">
-                        <li><a href="#!">Link 1</a></li>
-                        <li><a href="#!">Link 2</a></li>
-                        <li><a href="#!">Link 3</a></li>
-                        <li><a href="#!">Link 4</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    
-        <div className="footer-copyright text-center py-3">© 2020 Copyright:
-            <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-        </div>
-    
-    </footer>  
-    );
-} 
+  return (
+    <footer className="page-footer font-small blue pt-4">
+      <div className="container-fluid text-center text-md-left">
+        <div className="row">
+          <div className="col-md-6 mt-md-0 mt-3">
+            <h5 className="text-uppercase">Footer Content</h5>
+            <p>
+              Here you can use rows and columns to organize your footer content.
+            </p>
+          </div>
 
-export default Footer
+          <hr className="clearfix w-100 d-md-none pb-0" />
+
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled">
+              <li><a href="#!">Link 1</a></li>
+              <li><a href="#!">Link 2</a></li>
+              <li><a href="#!">Link 3</a></li>
+              <li><a href="#!">Link 4</a></li>
+            </ul>
+          </div>
+
+          <div className="col-md-3 mb-md-0 mb-3 text-center">
+            <h5 className="text-uppercase">Our University</h5>
+            {/*Added the ISU CS logo hyperlink here */}
+            <a
+              href="https://cs.isu.edu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/images/ISULogo.png"
+                alt="Idaho State University Computer Science"
+                style={{
+                  height: "60px",
+                  marginTop: "10px",
+                  cursor: "pointer",
+                }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-copyright text-center py-3">
+        © 2025 Idaho State University | Computer Science Department
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
